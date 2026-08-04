22ad94d14abbef1e782aa1879d5c6ebb94af3ad4
412-20-nuttakit <54624@yupparaj.ac.th>
2026-08-04 10:47:29 +0700

    Create Vat_app.py

---
+++ Vat_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
+
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+
+vat = (price/100)*7
+net_price = price - vat
+
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+
+st.divider()
+
+st.write("ณัฏฐกิตติ์ จันทร์ศิริ เลขที่ 10  ม.4/12")
