2ee73fd078fd4661fbea372259d3f7a7b137df9a
412-20-nuttakit <54624@yupparaj.ac.th>
2026-09-01 10:23:30 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -5,7 +5,7 @@ if "ans1_val" not in st.session_state:
 if "ans2_val" not in st.session_state:
     st.session_state.ans2_val = ""
     
-    def reset_game():
+def reset_game():
     st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
     st.session_state.ans2_val = ""  # เคลียร์ค่าช่องข้อ 2
     st.session_state.start = time.time()  # เริ่มเวลาใหม่
