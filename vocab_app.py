89968c2f2e2da719ae7874030f0b70a88da8cdf6
412-20-nuttakit <54624@yupparaj.ac.th>
2026-09-08 10:45:11 +0700

    Update vocab_app.py

--- vocab_app.py
+++ vocab_app.py
@@ -7,11 +7,11 @@ if "ans1_val" not in st.session_state:
     st.session_state.ans1_val = ""
 if "ans2_val" not in st.session_state:
     st.session_state.ans2_val = ""
-if "ans1_val" not in st.session_state:
+if "ans3_val" not in st.session_state:
     st.session_state.ans3_val = ""
-if "ans2_val" not in st.session_state:
+if "ans4_val" not in st.session_state:
     st.session_state.ans4_val = ""
-if "ans1_val" not in st.session_state:
+if "ans5_val" not in st.session_state:
     st.session_state.ans5_val = ""
 
 def reset_game():
