81f8432e9b4003d7482e63354297518f6de382fd
412-20-nuttakit <54624@yupparaj.ac.th>
2026-09-08 09:02:14 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -73,7 +73,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
     time.sleep(1)
     st.rerun()
-
+    
 if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
